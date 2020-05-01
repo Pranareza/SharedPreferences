6f7d1d687203d51b846fa63f04aefdbcbf6a7202
Pranareza <pranareza040400@gmail.com>
2020-05-01 22:08:27 +0700

    First commit

---
+++ app/src/main/java/com/apps/pranareza/latihansharedpreference/HomeActivity.java
@@ -0,0 +1,49 @@
+package com.apps.pranareza.latihansharedpreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.apps.pranareza.R;
+import com.apps.pranareza.latihansharedpreferences.utils.Preferences;
+
+/*
+    Developed by Prana Reza Alviana - 10117192 - IF5
+    on 30-04-2020
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView txtKeluar;
+    private TextView txtName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        declareView();
+        txtKeluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Clear Set Preferences
+                Preferences.setLogout(getBaseContext());
+
+                //Pindah Halaman ke Login
+                startActivity(new Intent(getBaseContext(), LoginActivity.class));
+                finish();
+            }
+        });
+
+    }
+
+    private void declareView() {
+        txtKeluar = findViewById(R.id.txt_logout);
+        txtName = findViewById(R.id.txtName);
+
+        txtName.setText(Preferences.getRegisteredUser(getBaseContext()));
+    }
+}
