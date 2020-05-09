0d8cacd2ec5d628b5f5312e7f9442c321e25ecaa
Pranareza <pranareza040400@gmail.com>
2020-05-09 22:59:55 +0700

    First commit

--- MySelfApps/app/src/main/java/com/apps/myselfapps/IntroActivity.java
+++ MySelfApps/app/src/main/java/com/apps/myselfapps/IntroActivity.java
@@ -21,9 +21,9 @@ public class IntroActivity extends AppCompatActivity {
         //fill list screen
 
         List<ScreenItem> mList = new ArrayList<>();
-        mList.add(new ScreenItem("Fresh Food", "",R.drawable.eat_icon));
-        mList.add(new ScreenItem("Nice Code", "",R.drawable.code_icon));
-        mList.add(new ScreenItem("Sleep For Rest", "",R.drawable.sleep_icon));
+        mList.add(new ScreenItem("Fresh Food", "Diawali dengan makan yang cukup!!!",R.drawable.eat_icon));
+        mList.add(new ScreenItem("Nice Code", "Setelah kenyang, mari kita mulai ngoding!!!",R.drawable.code_icon));
+        mList.add(new ScreenItem("Sleep For Rest", "Selesai ngoding, alangkah baiknya kita tidur untuk istirahat!!!",R.drawable.sleep_icon));
 
         //setup viewpager
         screenPager = findViewById(R.id.screen_viewpager);
