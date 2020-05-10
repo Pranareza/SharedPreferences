57e7900dc0e93989a4dc4feae71133675e977dde
Pranareza <pranareza040400@gmail.com>
2020-05-10 07:11:47 +0700

    First commit

--- MySelfApps/app/src/main/java/com/apps/myselfapps/IntroActivity.java
+++ MySelfApps/app/src/main/java/com/apps/myselfapps/IntroActivity.java
@@ -3,10 +3,14 @@ package com.apps.myselfapps;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TableLayout;
 
@@ -21,12 +25,14 @@ public class IntroActivity extends AppCompatActivity {
     IntroViewPagerAdapter introViewPagerAdapter;
     TabLayout tabIndicator;
     Button btnNext;
-    int position = 0 ;
+    int position = 0;
+    Button btnGetStarted;
+    Animation btnAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+
         //Buat activity full screen
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -34,12 +40,24 @@ public class IntroActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 
+        // when this activity is about to be lunch we need to check if its opened before or not
 
-        //hide the action bar
-        getSupportActionBar().hide();
+        if (restorePrefData()) {
+
+            Intent homeActivity = new Intent(getApplicationContext(),HomeActivity.class);
+            startActivity(homeActivity);
+            finish();
+        }
+
+
+        setContentView(R.layout.activity_intro);
+
+
 
         //ini views
         btnNext = findViewById(R.id.btn_next);
+        btnGetStarted = findViewById(R.id.btn_get_started);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.button_animation);
         tabIndicator = (TabLayout) findViewById(R.id.tab_indicator);
 
 
@@ -59,6 +77,95 @@ public class IntroActivity extends AppCompatActivity {
         tabIndicator.setupWithViewPager(screenPager);
 
         //next button click listener
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                position = screenPager.getCurrentItem();
+                if (position < mList.size()){
+
+                    position++;
+                    screenPager.setCurrentItem(position);
+                }
+
+                if (position == mList.size()-1) { // when we rech to the last screen
+
+                    //TODO : show the GETSTARTED Button and the next button
+
+                    loadLastScreen();
+
+                }
+            }
+        });
+
+        // tablayout add chance listener
+        tabIndicator.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition() == mList.size()-1) {
+
+                    loadLastScreen();
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        // Get Started button click listener
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //open home activity
+                Intent homeActivity = new Intent(getApplicationContext(),HomeActivity.class);
+                startActivity(homeActivity);
+
+                savePrefsData();
+                finish();
+
+            }
+        });
+    }
+
+    private boolean restorePrefData() {
+
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        Boolean isIntroActivityOpenedBefore = pref.getBoolean("isIntroOpened",false);
+        return isIntroActivityOpenedBefore;
+
+    }
+
+    private void savePrefsData() {
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("isIntroOpened", true);
+        editor.commit();
+    }
+
+    //show the GETSTARTED Button and the next button
+    private void loadLastScreen() {
+
+        btnNext.setVisibility(View.INVISIBLE);
+        btnGetStarted.setVisibility(View.VISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
+        // TODO : ADD an animation the getstarted button
+        // setup animation
+        btnGetStarted.setAnimation(btnAnim);
+
     }
 }
 
